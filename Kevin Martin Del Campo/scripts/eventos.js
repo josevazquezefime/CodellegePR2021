5a4a97d3c4cef0c6ed7ead892bf1ed9385f0caf9
KMCF <kevin.martin@softtek.com>
2021-03-26 09:40:55 -0600

    Actividades Curso

--- Kevin Martin Del Campo/scripts/eventos.js
+++ Kevin Martin Del Campo/scripts/eventos.js
@@ -1,59 +1,40 @@
-var productos = [
-    {
-        name: "iPhone 12 Pro Max",
-        brand: "Apple",
-        description: "The new 2020 iPhone 12. This Max Pro version has a better camera",
-        image: "https://images-na.ssl-images-amazon.com/images/I/71fJ-gmBZtL._AC_SX466_.jpg",
-        price: 1800.50,
-        stock: 15
-    },
-    {
-        name: "Mi 11",
-        brand: "Xiaomi",
-        description: "The New 2021 Xiaomi Mi 11",
-        image: "https://cdn.shopify.com/s/files/1/0512/1694/1248/products/CapturadePantalla2021-01-03ala_s_9.30.09_800x.png",
-        price: 800,
-        stock: 16
-    },
-    {
-        name: "iPhone 12 Pro Max",
-        brand: "Apple",
-        description: "The new 2020 iPhone 12. This Max Pro version has a better camera",
-        image: "https://images-na.ssl-images-amazon.com/images/I/71fJ-gmBZtL._AC_SX466_.jpg",
-        price: 1800.50,
-        stock: 15
-    },
-    {
-        name: "Mi 11",
-        brand: "Xiaomi",
-        description: "The New 2021 Xiaomi Mi 11",
-        image: "https://cdn.shopify.com/s/files/1/0512/1694/1248/products/CapturadePantalla2021-01-03ala_s_9.30.09_800x.png",
-        price: 800,
-        stock: 17
-    }
-];
+var productos = [];
 
-$(function(){
-    for (var i = 0; i < productos.length; i++) {
-        const producto = productos[i];
-        $('body').append(
-        `<div class="card mb-3 producto" onclick="MostrarDetalleProducto(${i})">
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="${producto.image}">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title"> ${producto.brand} - ${producto.name}</h5>
-                        <p class="card-text">${producto.description}</p>
-                        <p class="card-text">
-                            <small class="text-muted">In Stock: ${producto.stock} units</small>
-                        </p>
-                    </div>
-                </div>
-            </div>
-         </div>`);
-    }
+$(function () {
+
+    //Asynchronous Javascript And XML
+    var AJAX = new XMLHttpRequest(); //Estado inicial 0
+    AJAX.onreadystatechange = function () {
+        //State 3: Downloading
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+                productos = JSON.parse(this.responseText);
+                for (var i = 0; i < productos.length; i++) {
+                    const producto = productos[i];
+                    $('body').append(
+                        `<div class="card mb-3 producto" onclick="MostrarDetalleProducto(${i})">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img src="${producto.image}">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title"> ${producto.brand} - ${producto.name}</h5>
+                                    <p class="card-text">${producto.description}</p>
+                                    <p class="card-text">
+                                        <small class="text-muted">In Stock: ${producto.stock} units</small>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                     </div>`);
+                }
+            }
+        }
+        //State 4: Finish
+    };
+    AJAX.open('GET', 'http://localhost:666/products/all'); //State: 1
+    AJAX.send(); //State: 2
 });
 
 function MostrarDetalleProducto(productIndex) {
