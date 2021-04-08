504ff2d89e4daf9c5f4155ba6f61ab59f7f5ece4
josevazquezefime <78818230+josevazquezefime@users.noreply.github.com>
2021-04-08 12:28:46 -0500

    postman
    postman

---
+++ José Vázquez/JoseVazquez/desarrolla.store/service.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const PORT = 666;
+
+const uri = "mongodb+srv://joseV:zxn3w100QWERTY@cluster0.fbjan.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, error => {
+    if (error) {
+        console.log('Error connecting to the database');
+        console.log(error);
+    } else {
+        console.log('Succesfully connected to the mongo batabase server (Cluster)');
+        console.log('Server cluster: ' + mongoose.connection.host);
+        console.log('Server Cluster Port: ' + mongoose.connection.port);
+    }
+});
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+var routerUsers = require('./routers/users')
+app.use('/users', routerUsers);
+
+app.listen(PORT);
