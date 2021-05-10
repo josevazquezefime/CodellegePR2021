d147815df7348e6b434f57e7fb20bb0002a5cd41
Al-Garcia <78818180+Al-Garcia@users.noreply.github.com>
2021-05-10 09:45:47 -0500

    .
    .

---
+++ Luis Alberto Garcia de los Santos/scripts/desarrolla.Store/public/Store/src/components/store/store.ts
@@ -0,0 +1,14 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'store', //Asignar un nombre de etiqueta, único
+  templateUrl: './store.html', //Asignar la ruta del archivo .html que represente esta vista
+  styleUrls: ['./store.css'] //Un arreglo con las rutas de los CSS que queremos en este componente
+})
+//Debemos asignarle el nombre de nuestro componente.
+//Ejemplo: Si se llama catalogo.component.ts, debemos exportar CatalogoComponent
+export class StoreComponent { //Cambiar el nombre de AppComponent por el del nuestro
+
+}
--- Luis Alberto Garcia de los Santos/scripts/desarrolla.Store/public/Store/src/main.ts
+++ Luis Alberto Garcia de los Santos/scripts/desarrolla.Store/public/Store/src/main.ts
@@ -1,12 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { StoreModule } from './components/store/store.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(StoreModule).catch(err => console.error(err));
