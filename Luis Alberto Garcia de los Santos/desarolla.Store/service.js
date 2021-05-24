a9a934a224474104ad61ba0935694a15f5bfef0a
Al-Garcia <78818180+Al-Garcia@users.noreply.github.com>
2021-05-24 07:39:46 -0500

    .
    .

--- Luis Alberto Garcia de los Santos/desarolla.Store/service.js
+++ Luis Alberto Garcia de los Santos/desarolla.Store/service.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const PORT = 666;
 
-const uri = "mongodb+srv://gasl:asdfghjklñ@cluster0.yfmf7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://kmcf:spgg2021@cluster0.lh9dp.mongodb.net/SPGG?retryWrites=true&w=majority";
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
